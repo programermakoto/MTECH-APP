2960f345d3ce52bb0aa51daf5676f68faded6e29
Terauchi Makoto <makoto@Terauchis-MacBook-Air.local>
2024-10-17 10:14:30 +0900

    101603

--- components/auth/AuthServerButton.tsx
+++ components/auth/AuthServerButton.tsx
@@ -1,17 +1,17 @@
 import { cookies } from "next/headers";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { Session } from '@supabase/auth-helpers-nextjs'; // auth-helpersからSessionをインポート
+// import { Session } from '@supabase/auth-helpers-nextjs'; // auth-helpersからSessionをインポート
 import AuthClientButton from "./AuthClientButton";
 
 const AuthServerButton = async () => {
     const supabase = createServerComponentClient({ cookies });
-    const { data: { session } }: { data: { session: Session | null } } = await supabase.auth.getSession();
-
-    // sessionがnullかどうかをチェック
-    if (!session) {
-        return <div>Not signed in</div>;
-    }
-
+    // const { data: { session } }: { data: { session: Session | null } } = await supabase.auth.getSession();
+    const { data: user } = await supabase.auth.getSession();
+    // if (!session) {
+    //     return <div>Not signed in</div>;
+    // }
+    const session = user.session;
+    
     return (
         <AuthClientButton session={session} />
     );
