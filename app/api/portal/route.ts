6f6ced03b327f621a38f6e41c16df204c9bb07c8
Terauchi Makoto <makoto@Terauchis-MacBook-Air.local>
2024-10-21 16:29:01 +0900

    500error対応1

--- app/api/portal/route.ts
+++ app/api/portal/route.ts
@@ -28,10 +28,8 @@ import { supabaseRouteHandlerClient } from "@/utils/supabaseRouteHandlerCliient"
 // }
 
 
-
-
 export async function GET(
-    _req: NextRequest, // _req に変更
+    _req: NextRequest,
     { params }: { params: { priceId: string } }
 ) {
     const supabase = supabaseRouteHandlerClient();
@@ -45,29 +43,42 @@ export async function GET(
     const { data: stripe_customer_data } = await supabase
         .from("profile")
         .select("stripe_customer")
-        .eq("id", user.id) // user?.idをuser.idに変更
+        .eq("id", user.id)
         .single();
 
     if (!stripe_customer_data || !stripe_customer_data.stripe_customer) {
         return NextResponse.json({ error: "Invalid customer data" }, { status: 400 });
     }
 
-    const priceId = params.priceId;
+    const priceId = params.priceId; // priceIdを取得
+    console.log("Price ID:", priceId); // ここでpriceIdをログ出力
+
     const stripe = new initStripe(process.env.STRIPE_SECRET_KEY!);
 
-    const session = await stripe.checkout.sessions.create({
-        customer: stripe_customer_data.stripe_customer,
-        mode: "subscription",
-        payment_method_types: ["card"],
-        line_items: [{
-            price: priceId,
-            quantity: 1
-        }],
-        success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/payment/success`,
-        cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}/payment/cancelled`,
-    });
+    try {
+        const session = await stripe.checkout.sessions.create({
+            customer: stripe_customer_data.stripe_customer,
+            mode: "subscription",
+            payment_method_types: ["card"],
+            line_items: [{
+                price: priceId,
+                quantity: 1
+            }],
+            success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/payment/success`,
+            cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}/payment/cancelled`,
+        });
 
-    return NextResponse.json({
-        id: session.id
-    });
+        return NextResponse.json({
+            id: session.id
+        });
+    }catch (error) {
+        console.error("Error creating Stripe session:", error);
+    
+        if (error instanceof Error) {
+            return NextResponse.json({ error: error.message || "Failed to create session" }, { status: 500 });
+        } else {
+            return NextResponse.json({ error: "Unknown error occurred" }, { status: 500 });
+        }
+    }
+
 }
