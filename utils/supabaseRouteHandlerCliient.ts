ea1926be6907a9c2363e96a6b1b9bb5cd932c44e
Terauchi Makoto <makoto@Terauchis-MacBook-Air.local>
2024-10-24 17:03:23 +0900

    OKOKOKOKOKOK2

--- utils/supabaseRouteHandlerCliient.ts
+++ utils/supabaseRouteHandlerCliient.ts
@@ -1,8 +1,20 @@
+// import { Database } from "@/lib/database.types";
+// import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+// import { cookies } from "next/headers"
+
+// export const  supabaseRouteHandlerClient =()=>{
+//     cookies().getAll();
+//     return createRouteHandlerClient<Database>({cookies});
+// }
+
+
 import { Database } from "@/lib/database.types";
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers"
+import { cookies } from "next/headers";
 
-export const  supabaseRouteHandlerClient =()=>{
-    cookies().getAll();
-    return createRouteHandlerClient<Database>({cookies});
-}
+export const supabaseRouteHandlerClient = () => {
+    const cookieStore = cookies();  // Cookieストアを取得
+    return createRouteHandlerClient<Database>({
+        cookies: () => cookieStore  // cookiesオブジェクトを渡す
+    });
+};
