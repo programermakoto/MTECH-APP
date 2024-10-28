2250c4bab2967f1814a8c7fd73c9b2bdd7cafa13
Terauchi Makoto <makoto@Terauchis-MacBook-Air.local>
2024-10-28 15:20:11 +0900

    1028-5

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,13 @@
-
 import { SupabaseClient } from "@supabase/auth-helpers-nextjs";
 import Link from "next/link";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { supabaseServer } from "@/utils/supabaseServer";
 import { Database } from "@/lib/database.types";
 
-
-
 const getAllLessons = async (supabase: SupabaseClient<Database>) => {
   const { data: lessons } = await supabase.from("lesson").select("*");
-  return lessons
-}
-
-
+  return lessons;
+};
 
 export default async function Home() {
   const supabase = supabaseServer();
@@ -22,8 +17,13 @@ export default async function Home() {
     <main className="w-full max-w-7xl mx-auto my-16 ">
       <div className="w-full flex flex-wrap gap-2">
         {lessons?.map((Lesson) => (
-          <Link href={`/${Lesson.id}`} key={Lesson.id} className="w-full sm:w-1/2 md:w-1/3" style={{ flex: '0 0 calc(100% - 0.5rem)' }}> {/* 1/3の幅でカードを表示 */}
-            <Card className="w-full bg-gray-200" style={{ minHeight: '30vh' }}> {/* カードの幅を100%に設定 */}
+          <Link
+            href={`/${Lesson.id}`}
+            key={Lesson.id}
+            className="w-full sm:w-1/2 md:w-1/3"
+            style={{ flex: '0 0 calc(33.333% - 0.5rem)' }}
+          >
+            <Card className="w-full bg-gray-200" style={{ minHeight: '30vh' }}>
               <CardHeader>
                 <CardTitle>{Lesson.title}</CardTitle>
               </CardHeader>
@@ -35,8 +35,5 @@ export default async function Home() {
         ))}
       </div>
     </main>
-
-
   );
 }
-
