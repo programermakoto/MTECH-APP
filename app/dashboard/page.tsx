94f75813de08af1a532ebccbb0339bb97c1fd36b
Terauchi Makoto <makoto@Terauchis-MacBook-Air.local>
2024-11-12 15:47:31 +0900

    1112

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -26,11 +26,20 @@ const Dashboard = async () => {
                         ? `プラン契約中です。: ${profile.interval} プラン`
                         : "プラン未加入です。"}
                 </div>
-                <p className="text-center p-3 bg-white mt-6 rounded-lg  shadow-lg">
+                <p className="text-center p-3 bg-white mt-6 mb-6 rounded-lg  shadow-lg">
                     公式ラインお問い合わせは<a href="https://line.me/R/ti/p/@527qtybu?oat_content=url&ts=08060343" className=" mb-6text-red-500">こちら</a>から
                     <SubscriptionManagementButton />
                 </p>
-                
+                <p className="text-center p-4 bg-white mt-6 rounded-lg shadow-md space-y-3">
+                    <span className="text-gray-700">
+                        ITスクールの会員の方は
+                        <a href="https://mtech-it.com/blog/secret/index.html" className="text-red-500 hover:underline ml-1">
+                            こちら
+                        </a>
+                        から
+                    </span>
+                    <SubscriptionManagementButton />
+                </p>
             </div>
 
 
